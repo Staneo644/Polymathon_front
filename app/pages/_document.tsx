8c2a4a039ff656cf8a1beaae489022793a468801
Staneo644 <aurele.josserand@gmail.com>
2023-10-20 10:50:47 +0200

    petit commit avant eslint

---
+++ app/pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { JSX } from 'react';
+
+
+
+export default class MyDocument extends Document {
+  render(): JSX.Element {
+    
+    return (
+      <Html lang="en">
+      <Head>
+        <link rel="manifest" href="/public/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png"></link>
+        <meta name="theme-color" content="#fff" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+  }
+}
